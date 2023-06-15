8e63523bb68128a2791c7cab5a67a649ce6a047d
Iliad NUC <iliad@nuc>
2023-06-15 16:59:33 -0700

    do not export the paths

--- launcher/launch_robot.sh
+++ launcher/launch_robot.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-export CONDA_PREFIX=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
-# export LIBRARY_PATH=${CONDA_PREFIX}/lib
-export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib
+CONDA_PREFIX=${CONDA_PREFIX:-"$(dirname $(which conda))/../"} \
+LD_LIBRARY_PATH=${CONDA_PREFIX}/lib \
 launch_robot.py \
 --config-path ${PWD}/conf \
 --config-name robot_launch.yaml \
